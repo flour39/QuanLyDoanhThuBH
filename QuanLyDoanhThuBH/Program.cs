0842359a31f513d9c717efd77ef3e1d3074d6f69
HEHE-PC <nhoa54584@gmai.com>
2025-09-13 14:43:32 +0700

    Add project files.

---
+++ QuanLyDoanhThuBH/Program.cs
@@ -0,0 +1,15 @@
+﻿using QuanLyDoanhThuBH.Data;
+using Microsoft.EntityFrameworkCore; // Add this using directive
+
+var builder = WebApplication.CreateBuilder(args);
+
+// đăng ký DbContext
+builder.Services.AddDbContext<QuanLyContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// services MVC
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+// ... pipeline
+app.Run();
